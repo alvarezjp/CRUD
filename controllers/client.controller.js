1297d481db2d5a5e64af6df576a9241d5dfa0fe4
alvarezjp <6.jesus.a@gmail.com>
2022-08-10 05:56:20 -0400

    hasta elmodulo 3 video 2 min 15:30

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,8 +1,10 @@
 import { clientService } from "../service/client-service.js";
 
+
 console.log(clientService);
 
-const crearNuevaLinea = (nombre, email) => {
+const crearNuevaLinea = (nombre, email, id) => {
+    console.log(id);
     const linea = document.createElement("tr")
     const contenido = `
       <td class="td" data-td>
@@ -21,8 +23,7 @@ const crearNuevaLinea = (nombre, email) => {
           <li>
             <button
               class="simple-button simple-button--delete"
-              type="button"
-            >
+              type="button" id="${id}">
               Eliminar
             </button>
           </li>
@@ -30,17 +31,26 @@ const crearNuevaLinea = (nombre, email) => {
       </td>
     `;
     linea.innerHTML = contenido;
+    const btn = linea.querySelector("button");
+    btn.addEventListener("click",() => {
+      const id = btn.id;
+      console.log("el click",id)
+    })
     return linea;
   };
   
   const table = document.querySelector("[data-table]");
 
-  clientService.listaClientes().then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+  clientService
+  .listaClientes()
+  .then((data) => {
+    data.forEach(({nombre,email,id}) => { // esto se llama destructuring en vez de poner entre el parentecis amarillo agregar una variable, se destructura altiro
+      const nuevaLinea = crearNuevaLinea(nombre,email,id);
       table.appendChild(nuevaLinea);
     });
   }).catch((error)=>alert("HAY PROBLEMAS"));
+
+
 
 
 // CRUD   - Metodos http

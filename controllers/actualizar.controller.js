e0813c650f0d47468d91dab7eb4ef291ca5271fb
alvarezjp <6.jesus.a@gmail.com>
2022-08-12 05:10:40 -0400

    termino del curso, lo ultimo agregado fue el async

--- controllers/actualizar.controller.js
+++ controllers/actualizar.controller.js
@@ -2,35 +2,46 @@ import { clientService } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]")
 
-const obtenerInformacion = () => {
+const obtenerInformacion = async () => {
     const url = new URL(window.location);
     const id = url.searchParams.get("id");
 
-    if(id == null){
+    if (id == null) {
         window.location.href = "/screens/error.html"
     }
 
     const nombre = document.querySelector("[data-nombre]");
     const email = document.querySelector("[data-email]");
-   
-    clientService.detalleCliente(id).then(perfil =>{
+
+    try {
+        const perfil = await clientService.detalleCliente(id);
+        
+        if(perfil.nombre && perfil.email){
         nombre.value = perfil.nombre;
         email.value = perfil.email;
-    });
+    }else{
+        throw new Error();
+    }
+    } catch (error) {
+        window.location.href = "/screens/error.html"
+    }
 
-}
+
+};
+
+
 
 obtenerInformacion();
 
-formulario.addEventListener("submit",(evento) =>{
+formulario.addEventListener("submit", (evento) => {
     evento.preventDefault();//prevenir el funcionamiento normal del formulario
     const url = new URL(window.location);
     const id = url.searchParams.get("id");
 
     const nombre = document.querySelector("[data-nombre]").value;
     const email = document.querySelector("[data-email]").value;
-   
-    clientService.actualizarCliente(nombre,email,id).then(() => {
+
+    clientService.actualizarCliente(nombre, email, id).then(() => {
         window.location.href = "/screens/edicion_concluida.html"
     });
 })
